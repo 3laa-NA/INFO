b31c5b3eda22ebfa00de87ed0b30e09600def9a2
3laa-NA <125448375+3laa-NA@users.noreply.github.com>
2023-09-20 11:07:05 +0200

    Create Segment.java

---
+++ IN02/TME01/Segment.java
@@ -0,0 +1,16 @@
+public class Segment {
+	private int x,y;
+	public Segment(int extX,int extY){
+		x=extX;
+		y=extY;
+	}
+	public int longueur(){
+		if(x>y)
+			return x-y;
+		else
+			return y-x;
+	}
+	public String toString(){
+		return "Segment ["+ x +","+ y +"]";
+	}
+}
