f6c54438644b9d4b601e8e5e9e36ad1c8063af13
3laa-NA <125448375+3laa-NA@users.noreply.github.com>
2023-09-27 15:40:52 +0000

    ...

--- IN02_(java)/TME03/Point.java
+++ IN02_(java)/TME03/Point.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 public class Point {
     private double posx, posy;
 
@@ -13,8 +15,22 @@ public class Point {
 
     public String toString (){
 
-        return "(" + posx + ", " + posy + ")" ;
+        DecimalFormat df = new DecimalFormat("#.##"); 
+        return "(" + df.format(posx) + ", " + df.format(posy) + ")" ;
     }
    
+    public double distance(Point p){
+        double x1 = this.posx;
+        double y1 = this.posy;
+        double x2 = p.posx;
+        double y2 = p.posy;
+
+        return Math.sqrt( Math.pow((x2-x1),2.0) + Math.pow((y2-y1),2.0) );
+    }
+
+    public void deplaceToi(int newx, int newy){
+        this.posx = newx;
+        this.posy = newy;
+    }
 
 }
---
+++ IN02_(java)/TME03/TestTriangle.java
@@ -0,0 +1,12 @@
+public class TestTriangle{
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        Point p2 = new Point();
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+
+        System.out.println(p1.distance(p2));
+
+    }
+}
