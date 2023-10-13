4a702961216ca9141b8f492bd2ee5cd3e7f748f1
3laa-NA <125448375+3laa-NA@users.noreply.github.com>
2023-10-13 13:51:42 +0000

    ...

---
+++ IN20_(ISS)_(C)/TME05/exo1/jungle.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Choisissez la langue (VF ou VO) :"
+read langue
+
+# Vérifie si l'utilisateur a saisi VF ou VO
+if [ "$langue" != "VF" ] && [ "$langue" != "VO" ]; then
+    echo "Langue non reconnue. Utilisez VF ou VO."
+    exit 1
+fi
+
+# Définir le séparateur de champ pour le fichier CSV
+IFS=","
+
+
+while read -r animal cri_fr cri_en; do
+    if [ "$langue" == "VF" ]; then
+        cowsay -f $animal "$cri_fr"
+    else
+        cowsay -f $animal "$cri_en"
+    fi
+done < "/blabla.csv"
+
---
+++ install.pl
@@ -0,0 +1,2 @@
+s,%BANGPERL%,!/usr/bin/perl,;
+s,%PREFIX%,/bin/sh "/workspaces/INFO/IN20_(ISS)_(C)/TME05/cowsay/install.sh",;
