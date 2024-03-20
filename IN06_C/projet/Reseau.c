46aced836517d31c686301d09252c4e5c3dd72ba
3laa-NA <125448375+3laa-NA@users.noreply.github.com>
2024-03-20 14:13:52 +0000

    ...

--- IN06_C/projet/Reseau.c
+++ IN06_C/projet/Reseau.c
@@ -153,14 +153,11 @@ void ecrireReseau(Reseau *R, FILE *f){
         CellNoeud *tmp_v = tmp_n->nd->voisins;
 
         while(tmp_v){
-            /*char *l;
-            sprintf(l, "l %d %d\n", tmp_n->nd->num, tmp_v->nd->num); // pour verifier si la liason a été noté déjà
 
-            if(!strstr(lia, l)){
+            if(tmp_v->nd->num < tmp_n->nd->num){
                 sprintf(lia + strlen(lia), "l %d %d\n", tmp_v->nd->num, tmp_n->nd->num);
-            }*/
-
-            sprintf(lia + strlen(lia), "l %d %d\n", tmp_v->nd->num, tmp_n->nd->num);
+            }
+            
             tmp_v = tmp_v->suiv;
         }
 
